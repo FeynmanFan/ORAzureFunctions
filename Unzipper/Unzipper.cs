ca46d6f76b1bb4d7b15e1d1c442fe80c459d98b5
Chris Behrens <cbehrens@gmail.com>
2019-09-21 13:00:26 -0500

    Archive zip state

--- Unzipper/Unzipper.cs
+++ Unzipper/Unzipper.cs
@@ -10,18 +10,21 @@ namespace Unzipper
     public static class Function1
     {
         [FunctionName("Function1")]
-        public static void Run([BlobTrigger("incoming/{name}", Connection = "zipStorage")]Stream myBlob, string name, ILogger log)
+        public static void Run(
+            [BlobTrigger("incoming/{name}", Connection = "zipStorage")]Stream myBlob, 
+            [Blob("archive/{DateTime}/{name}", FileAccess.Write, Connection ="zipStorage")]Stream archiveBlob,
+            string name, ILogger log)
         {
             log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
+
+            myBlob.CopyTo(archiveBlob);
 
             var currentDirectory = Directory.GetCurrentDirectory();
 
             var extracted = Path.Combine(currentDirectory, "extracted");
 
-            if (!Directory.Exists(extracted))
-            {
-                Directory.CreateDirectory(extracted);
-            }
+            Directory.Delete(extracted);
+            Directory.CreateDirectory(extracted);
 
             ZipArchive archive = new ZipArchive(myBlob);
             foreach (ZipArchiveEntry entry in archive.Entries)
