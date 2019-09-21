5ceface269b65bb671c01b7ad14a210938cdc450
Chris Behrens <cbehrens@gmail.com>
2019-09-21 13:22:33 -0500

    Master unzipper state

--- Unzipper/Unzipper.cs
+++ Unzipper/Unzipper.cs
@@ -1,8 +1,8 @@
 using System;
 using System.IO;
 using System.IO.Compression;
+using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 
 namespace Unzipper
@@ -10,26 +10,31 @@ namespace Unzipper
     public static class Function1
     {
         [FunctionName("Function1")]
-        public static void Run(
+        public async static Task Run(
             [BlobTrigger("incoming/{name}", Connection = "zipStorage")]Stream myBlob, 
             [Blob("archive/{DateTime}/{name}", FileAccess.Write, Connection ="zipStorage")]Stream archiveBlob,
-            string name, ILogger log)
+            string name, ILogger log, Binder binder)
         {
             log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
 
             myBlob.CopyTo(archiveBlob);
 
-            var currentDirectory = Directory.GetCurrentDirectory();
-
-            var extracted = Path.Combine(currentDirectory, "extracted");
-
-            Directory.Delete(extracted);
-            Directory.CreateDirectory(extracted);
-
             ZipArchive archive = new ZipArchive(myBlob);
             foreach (ZipArchiveEntry entry in archive.Entries)
             {
-                entry.ExtractToFile(Path.Combine(extracted, entry.FullName));
+                var attributes = new Attribute[]
+                {
+                    new BlobAttribute($"extracted/{entry.Name}", FileAccess.Write),
+                    new StorageAccountAttribute("zipStorage")
+                };
+
+                using (var extractedStream = await binder.BindAsync<Stream>(attributes))
+                {
+                    using (var entryStream = entry.Open())
+                    {
+                        entryStream.CopyTo(extractedStream);
+                    }
+                }
             }
         }
     }
